3be43c10fc7e83ef222a7434a44b4a2cddf39ae0
Mahendra <mahendrapachipala123@gmail.com>
2023-10-19 19:17:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header className="header">
+        <h1>Naga Mahendra</h1>
       </header>
+
+      <div className="content">
+        <section className="section">
+          <h2>Introduction</h2>
+          <p>
+            I'm Naga Mahendra, a B.Tech student with a passion for Artificial Intelligence and Machine Learning.
+          </p>
+        </section>
+
+        <section className="section">
+          <h2>Skills</h2>
+          <ul>
+            <li>Java</li>
+            <li>Python</li>
+            <li>Data Structures</li>
+            <li>HTML & CSS</li>
+            <li>SQL</li>
+            <li>JavaScript</li>
+          </ul>
+        </section>
+
+        <section className="section">
+          <h2>Projects</h2>
+          <ul>
+            <li>Fraud Detection</li>
+            <li>Image Recognition</li>
+            <li>Hotel Booking Website</li>
+            <li>Home Automation</li>
+          </ul>
+        </section>
+
+        <section className="section">
+          <h2>Contact</h2>
+          <a href="mailto:mahendrapachipala123@gmail.com">Email me</a>
+        </section>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; // Add this export statement to make the component available for use in other parts of your application
